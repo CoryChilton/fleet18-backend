ff6ae6cb7dd512617a4ca14067d5be58ba55e451
Cory Chilton <corychilton@Corys-MacBook-Air.local>
2025-09-25 18:08:00 -0700

    remove person model

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from users.models import Person, User, Racer
+from users.models import User, Racer
 
 # Register your models here.
-admin.site.register(Person)
 admin.site.register(User)
 admin.site.register(Racer)
--- src/users/models.py
+++ src/users/models.py
@@ -3,14 +3,13 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class Person(models.Model):
+class Racer(models.Model):
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
 
 
 class User(AbstractUser):
-    person = models.OneToOneField(Person, on_delete=models.CASCADE, related_name="user")
+    racer = models.OneToOneField(Racer, on_delete=models.SET_NULL, related_name="user", null=True)
 
 
-class Racer(models.Model):
-    person = models.OneToOneField(Person, on_delete=models.CASCADE, related_name="racer")
+
