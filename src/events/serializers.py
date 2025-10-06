ac4941bf63d45f4577328aa2f7fdf8ee78a2469a
Cory Chilton <corychilton@Corys-MacBook-Air.local>
2025-10-05 20:51:05 -0700

    add events apis

---
+++ src/events/serializers.py
@@ -0,0 +1,24 @@
+from .models import Event
+from rest_framework import serializers
+from django.utils import timezone
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+    def validate_event_time(self, value):
+        """"
+        Check that the expiration timestamp is greater than current time
+        """
+        if value < timezone.now():
+            raise serializers.ValidationError("Event time cannot be in the past.")
+        return value
+    
+    def validate_entry_fee(self, value):
+        """
+        Check the entry fee is non-negative
+        """
+        if value < 0:
+            raise serializers.ValidationError("Entry fee must be non-negative.")
+        return value
--- src/events/views.py
+++ src/events/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from .models import Event
+from rest_framework import viewsets, permissions
+from .serializers import EventSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.utils import timezone
 
-# Create your views here.
+class EventViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for announcements.
+    """
+    queryset = Event.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = EventSerializer
