b2336dc4c6e71a79dfba3b0552d167b0017bd290
Cory Chilton <corychilton@Corys-MacBook-Air.local>
2025-10-05 18:16:13 -0700

    add validation on announcement post calls

--- src/announcements/serializers.py
+++ src/announcements/serializers.py
@@ -1,7 +1,17 @@
 from .models import Announcement
 from rest_framework import serializers
+from django.utils import timezone
 
 class AnnouncementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Announcement
         fields = '__all__'
+
+    def validate_expiration_timestamp(self, value):
+        """"
+        Check that the expiration timestamp is greater than current time
+        """
+        if value < timezone.now():
+            raise serializers.ValidationError("Expiration time cannot be in the past.")
+        return value
+    
