2d761002dad01ed2aca5322b93e52792442561e7
Cory Chilton <corychilton@Corys-MacBook-Air.local>
2025-10-14 20:43:24 -0700

    add EventPhoto model

--- src/photos/models.py
+++ src/photos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import User
+from events.models import Event
+from django.utils import timezone
 
 # Create your models here.
+class EventPhoto(models.Model):
+    photo = models.ImageField(upload_to='event_photos/', height_field="height", width_field="width")
+    photo_taken_timestamp = models.DateTimeField(default=timezone.now)
+    upload_timestamp = models.DateTimeField(auto_now_add=True)
+    poster = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    caption = models.CharField(max_length=255, blank=True, null=True)
+    event = models.ForeignKey(Event, on_delete=models.PROTECT)
