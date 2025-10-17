27e361406e53759f40041be3373159a101b50246
Cory Chilton <corychilton@Corys-MacBook-Air.local>
2025-10-16 21:37:46 -0700

    add photo_taken_timestamp from exif data during upload

--- src/photos/views.py
+++ src/photos/views.py
@@ -4,6 +4,9 @@ from .serializers import EventPhotoSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.utils import timezone
+from PIL import Image, ExifTags
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 class EventPhotoViewSet(viewsets.ModelViewSet):
     """
@@ -14,3 +17,31 @@ class EventPhotoViewSet(viewsets.ModelViewSet):
         permissions.AllowAny
     ]
     serializer_class = EventPhotoSerializer
+
+    def perform_create(self, serializer):
+        photo_file = self.request.FILES.get('photo')
+        photo_taken_timestamp = None
+
+        if photo_file:
+            try:
+                photo = Image.open(photo_file)
+                exif = photo.getexif()
+
+                if exif:
+                    exif_data = {ExifTags.TAGS.get(k): v for k, v in exif.items() if k in ExifTags.TAGS}
+
+                    if 'DateTime' in exif_data:
+                        photo_taken_timestamp_str = exif_data['DateTime']
+                        photo_taken_timestamp = datetime.strptime(photo_taken_timestamp_str, '%Y:%m:%d %H:%M:%S')
+                        photo_taken_timestamp = photo_taken_timestamp.replace(tzinfo=ZoneInfo('America/Los_Angeles'))
+
+
+            except Exception as e:
+                print(f"EXIF read failed: {e}")
+
+
+        if photo_taken_timestamp:
+            serializer.save(photo_taken_timestamp=photo_taken_timestamp)
+        else:
+            serializer.save()
+
